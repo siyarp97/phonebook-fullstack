e41072667b21198e6a8a78aa1ae158987f186b0e
siyarp97 <siyarp97@gmail.com>
2025-11-15 17:43:35 +0300

    Index configured

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,10 @@
+import axios from "axios";
 const express = require('express')
 const app = express()
 
 app.use(express.static('dist'))
+
+const getAll = () => {
+    const req = axios.get('/')
+    return req.then(res => res.data)
+}
